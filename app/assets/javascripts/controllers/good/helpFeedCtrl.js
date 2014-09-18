92c342871a7ae6680c29cd4f3dc42b07735a5f66
crotten <wildrunts@gmail.com>
2014-09-18 11:17:10 -0400

    working on user controller

--- app/assets/javascripts/controllers/good/helpFeedCtrl.js
+++ app/assets/javascripts/controllers/good/helpFeedCtrl.js
@@ -1,24 +1,27 @@
 angular.module("helpFeed")
     .controller("helpFeedCtrl", function ($scope,$log,$location, $routeParams, $rootScope, $route, helpFeedSvc) {
-
+      $scope.currentUser = currentUser;
+      $scope.helps = [];
       helpFeedSvc.getHelps().then(function (helps) {
         $scope.helps = helps.data;
       });
 
-      helpFeedSvc.getHelp($routeParams.id).then(function (response) {
+      helpFeedSvc.getHelp($scope.currentUser.id).then(function (response) {
         $scope.singleHelp = response.data;
       });
 
-      $scope.addHelp = function (help) {
-          helpFeedSvc.addHelp({
-            date:new Date(),
-            problem:help.problem,
-            landmark:help.landmark,
-            comments: []
-          })
+
+      $scope.addHelp = function (helpObj) {
+        var newHelp = {
+          date:new Date(),
+          problem:helpObj.problem,
+          landmark:helpObj.landmark,
+          comments: []
+        };
+          helpFeedSvc.addHelp(newHelp)
           $scope.helps.push(newHelp);
-          $log.info(help);
-          $scope.help = {};
+          $log.info(newHelp);
+          $scope.newHelp = {};
         };
 
     });
--- app/assets/javascripts/controllers/good/helpFeedSvc.js
+++ app/assets/javascripts/controllers/good/helpFeedSvc.js
@@ -1,18 +1,19 @@
 angular.module("helpFeed")
     .factory("helpFeedSvc", function ($rootScope, $log, $http) {
 
-      var urlHelp = "/users.json";
+      var urlHelp = "/users/";
 
       var getHelps = function () {
-        return $http.get(urlHelp);
+        return $http.get("/");
       };
 
       var getHelp = function (id) {
-        return $http.get(urlHelp + "/" + id);
+        return $http.get(urlHelp + id + "/posts");
       };
 
-      var addHelp = function (help) {
-      $http.post(urlHelp + "/", help).then(function (response) {
+      var addHelp = function (help,id) {
+        console.log(help);
+      $http.post(urlHelp + id + "/posts" + "/new", help).then(function (response) {
         $rootScope.$broadcast("help:added");
         $log.info("help:added");
         });
